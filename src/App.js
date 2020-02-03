763c7515b6b8099f5dbc5bd086c19ac3a1846653
ttwkr <ttwkor@naver.com>
2020-02-03 16:57:11 +0900

    초기세팅 - 초기  component 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Product from "./component/product/product";
+import Order from "./component/order/order";
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <h1>HI</h1>
+      <Product></Product>
+      <Order></Order>
     </div>
   );
-}
+};
 
 export default App;
