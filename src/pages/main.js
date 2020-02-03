eaadf9c3919a8a3ded54d9645c9be951c37241a3
ttwkr <ttwkor@naver.com>
2020-02-03 17:11:29 +0900

    초기셋팅 - websocket 셋팅

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Product from "../component/product/product";
 import Order from "../component/order/order";
 
 const Main = () => {
+  useEffect(() => {
+    const ws = new WebSocket("ws://127.0.0.1:8000/ws/lastorder");
+
+    ws.onopen = () => {
+      console.log("connect");
+    };
+
+    ws.onmessage = e => {
+      const data = JSON.parse(e.data);
+      console.log(data);
+    };
+
+    ws.onclose = () => {
+      console.log("close");
+    };
+  }, []);
   return (
     <div>
       <Product></Product>
